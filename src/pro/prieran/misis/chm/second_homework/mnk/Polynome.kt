4103cd097eafc90e8da96b44d4dfeb67b4863569
PrieraN <prieran@ya.ru>
2016-11-24 21:45:32 +0300

    Вынос полиномов в МНК в численных методах

--- src/pro/prieran/misis/chm/second_homework/mnk/Polynome.kt
+++ src/pro/prieran/misis/chm/second_homework/mnk/Polynome.kt
@@ -5,30 +5,25 @@ import java.lang.Math.abs
 import java.lang.Math.pow
 
 class Polynome {
-    fun gramMatrix(points: List<Point>, m: Int): Array<Double> {
-        val a = Array(m, { i -> Array<Double>(m + 1, { j -> 0.0 }) })
-        for (i in 0..a.size - 1) {
-            for (j in 0..a[i].size - 2) {
-                for (k in 0..points.size - 1)
-                    a[i][j] += pow(points[k].x, (i + j).toDouble())
-            }
-        }
 
-        for (i in 0..a.size - 1) {
-            for (j in 0..points.size - 1)
-                a[i][a[i].size - 1] += pow(points[j].x, i.toDouble()) * points[j].y
+    fun function(from: Double, to: Double, coefs: Array<Double>, x: Double): Double {
+        var value = 0.0
+        for (i in 0..coefs.size - 1) {
+            value += coefs[i] * newPolynomial(from, to, i)(x)
         }
-        return gauss(a)
+        return value
     }
 
-    fun gramMatrix(points: List<Point>, from: Double, to: Double): Array<Double> {
+    fun gramMatrix(points: List<Point>): Array<Double> {
         val m = points.size
+        val from = 0.0
+        val to = 6.0
 
         val a = Array(m, { i -> Array<Double>(m + 1, { j -> 0.0 }) })
 
         for (i in 0..a.size - 1) {
             for (j in 0..a[i].size - 2) {
-                a[i][j] = scalarProduct(from, to, newPolynomial(from, to, i), newPolynomial(from, to, j))
+                a[i][j] = scalarProduct(points, newPolynomial(from, to, j), newPolynomial(from, to, i))
             }
         }
 
@@ -39,40 +34,7 @@ class Polynome {
     }
 
     fun newPolynomial(from: Double, to: Double, power: Int): (Double) -> Double {
-        val zero = { x: Double -> 1.0 }
-        val d1 = scalarProduct(from, to, { x -> x }, { x -> zero(x) }) /
-                scalarProduct(from, to, { x -> zero(x) }, { x -> zero(x) })
-        val first = { x: Double -> x - d1 }
-
-        if (power == 0) {
-            return zero
-        } else if (power == 1) {
-            return first
-        } else {
-            val currentMinusOne = zero
-            val current = first
-            val d2 = scalarProduct(from, to, { x -> x * current(x) }, { x -> current(x) }) /
-                    scalarProduct(from, to, { x -> current(x) }, { x -> current(x) })
-            val d3 = scalarProduct(from, to, { x -> x * current(x) }, { x -> currentMinusOne(x) }) /
-                    scalarProduct(from, to, { x -> currentMinusOne(x) }, { x -> currentMinusOne(x) })
-            var next = { x: Double -> x * current(x) - d2 * current(x) - d3 * currentMinusOne(x) }
-
-            for (i in 2..power) {
-                val currentMinusOne2 = current
-                val current2 = next
-                val d21 = scalarProduct(from, to, { x -> x * current2(x) }, { x -> current2(x) }) /
-                        scalarProduct(from, to, { x -> current2(x) }, { x -> current2(x) })
-                val d31 = scalarProduct(from, to, { x -> x * current2(x) }, { x -> currentMinusOne2(x) }) /
-                        scalarProduct(from, to, { x -> currentMinusOne2(x) }, { x -> currentMinusOne2(x) })
-                next = { x: Double -> x * current2(x) - d21 * current2(x) - d31 * currentMinusOne2(x) }
-            }
-
-            return next
-        }
-    }
-
-    fun scalarProduct(from: Double, to: Double, func1: (Double) -> Double, func2: (Double) -> Double): Double {
-        return integrate(from, to, func1, func2)
+        return { x: Double -> pow(x, power.toDouble()) }
     }
 
     fun scalarProduct(points: List<Point>, func: (Double) -> Double): Double {
@@ -83,40 +45,12 @@ class Polynome {
         return product
     }
 
-    fun gramMatrix(m: Int, func: Func, from: Double, to: Double): Array<Double> {
-        val a = Array(m, { i -> Array<Double>(m + 1, { j -> 0.0 }) })
-
-        for (i in 0..a.size - 1) {
-            for (j in 0..a[i].size - 2) {
-                a[i][j] = integrate(from, to, { x: Double -> pow(x, i.toDouble()) }, { x: Double -> pow(x, j.toDouble()) })
-            }
+    fun scalarProduct(points: List<Point>, func1: (Double) -> Double, func2: (Double) -> Double): Double {
+        var product = 0.0
+        for (point in points) {
+            product += func1(point.x) * func2(point.x)
         }
-
-        for (i in 0..a.size - 1) {
-            a[i][a[i].size - 1] = integrate(from, to, { x: Double -> pow(x, i.toDouble()) }, { x -> func.func(x) })
-        }
-        return gauss(a)
-    }
-
-    private fun integrate(from: Double, to: Double, func1: (Double) -> Double, func2: (Double) -> Double): Double {
-        val step = 1E-4
-
-        val func = { x: Double -> func1(x) * func2(x) }
-        var integral = 0.0
-        var current = from
-        while (true) {
-            current += step
-
-            integral += 4.0 * func(current) + 2 * func(current + step)
-
-            if (current > to) {
-                break
-            }
-        }
-
-        current *= step / 3.0
-
-        return integral
+        return product
     }
 
     private fun gauss(coefficients: Array<Array<Double>>): Array<Double> {
