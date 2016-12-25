b6a33aa62335460481af99e1561257a37ed79cab
PrieraN <prieran@ya.ru>
2016-12-25 21:12:59 +0300

    Монте-Карло v1.0

--- src/pro/prieran/misis/chm/monte_carlo/Main.java
+++ src/pro/prieran/misis/chm/monte_carlo/Main.java
@@ -1,89 +1,87 @@
 package pro.prieran.misis.chm.monte_carlo;
 
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.Arrays;
-import java.util.Locale;
 import java.util.function.Function;
 
 public class Main {
+    private static final int COUNT_OF_ALL_POINTS = 100_000;
+
     public static void main(String[] args) throws Exception {
+        manyDimensions();
+        oneDimension();
+    }
+
+    private static double func(double[] params) {
+        double x = params[0];
+        double y = params[1];
+        double z = params[2];
+        return Math.pow(x, 3) + Math.pow(y, 2) + Math.log(z);
+    }
+
+    private static void manyDimensions() {
         Pcg32 random = new Pcg32();
 
         final int countOfDimens = 3;
         final double EPS_BOX = 1E-4;
 
-        double[] from = {0, 0, 0};
-        double[] to = {2, 3, 4};
+        double[] from = {3, 2, 1};
+        double[] to = {5, 4, 3};
 
         double boxMax = Double.NEGATIVE_INFINITY;
-        double boxMin = Double.POSITIVE_INFINITY;
 
         double[] tempFrom = Arrays.copyOf(from, countOfDimens);
         for (int i = 0; i < countOfDimens; i++) {
             for (; tempFrom[i] <= to[i]; tempFrom[i] += EPS_BOX) {
                 boxMax = Math.max(boxMax, func(tempFrom));
-                boxMin = Math.min(boxMin, func(tempFrom));
             }
         }
 
-        final int countOfAllPoints = 100_000;
-        int countOfGoodPoints = 0; // Те, что под графиком
+        double boxSquare = 1.0;
+        boxSquare *= boxMax;
+        for (int i = 0; i < countOfDimens; i++) {
+            boxSquare *= (to[i] - from[i]);
+        }
 
-        for (int i = 0; i < countOfAllPoints; i++) {
+        int countOfGoodPoints = 0; // Те, что под графиком
+        for (int i = 0; i < COUNT_OF_ALL_POINTS; i++) {
             double[] point = new double[countOfDimens + 1];
             for (int j = 0; j < countOfDimens; j++) {
                 point[j] = from[j] + random.nextDouble((to[j] - from[j]));
             }
-            point[countOfDimens] = boxMin + random.nextDouble(boxMax - boxMin);
+            point[countOfDimens] = random.nextDouble(boxMax);
+
 
             double[] pointArg = Arrays.copyOf(point, countOfDimens);
             if (point[countOfDimens] <= func(pointArg)) {
                 countOfGoodPoints++;
             }
+
+//            if (i % (COUNT_OF_ALL_POINTS / 100) == 0) {
+//                double currSquare = boxSquare * countOfGoodPoints / i;
+//                System.out.printf(Locale.getDefault(), "%d;%f\n", i, currSquare);
+//            }
         }
 
-        double boxSquare = 1.0;
-        boxSquare *= (boxMax - boxMin);
-        for (int i = 0; i < countOfDimens; i++) {
-            boxSquare *= (to[i] - from[i]);
-        }
-
-        double square = boxSquare * countOfGoodPoints / countOfAllPoints;
-        System.out.println(square);
+        double square = boxSquare * countOfGoodPoints / COUNT_OF_ALL_POINTS;
+        System.out.println("Result: " + square);
     }
 
-    private static double func(double[] params) {
-        return Math.pow(params[0], 2) + Math.pow(params[1], 3) + Math.pow(params[2], 4);
-    }
-
-    private void oneDimension() throws FileNotFoundException {
+    private static void oneDimension() throws FileNotFoundException {
         Pcg32 random = new Pcg32();
 
-        Function<Double, Double> func = x -> Math.pow(x, 2);
+        final Function<Double, Double> func = x -> 3 + Math.sin(x) * Math.exp(Math.cos(x * x / 10));
+
         double from = 0.0;
-        double to = 2.0;
-        int countOfPoints = 100_000;
+        double to = 20.0;
 
-        PrintWriter writer = new PrintWriter("name.csv");
-
-        for (int k = 0; k < 20; k++) {
-            double average = 0.0;
-            for (int i = 0; i < countOfPoints; i++) {
-                double value = from + random.nextDouble(to - from);
-                average += func.apply(value);
-                if (i % 100 == 0) {
-                    double currAvg = average / (i + 1);
-                    currAvg *= (to - from);
-                    writer.format(Locale.getDefault(), "%d;%f\n", i, currAvg);
-                }
-            }
-            average /= countOfPoints;
-            average *= (to - from);
-            System.out.println(average);
+        double average = 0.0;
+        for (int i = 0; i < COUNT_OF_ALL_POINTS; i++) {
+            double value = from + random.nextDouble(to - from);
+            average += func.apply(value);
         }
-
-        writer.flush();
-        writer.close();
+        average /= COUNT_OF_ALL_POINTS;
+        average *= (to - from);
+        System.out.println("Result: " + average);
     }
 }
