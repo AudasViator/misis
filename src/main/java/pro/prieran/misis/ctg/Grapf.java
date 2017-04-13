da7e379c879c486ab0477e27ba5f8e5e732c4307
prieran <noreply@prieran.pro>
2017-04-13 13:51:38 +0300

    Kruskal некрасивый

--- src/main/java/pro/prieran/misis/ctg/Grapf.java
+++ src/main/java/pro/prieran/misis/ctg/Grapf.java
@@ -169,8 +169,6 @@ public class Grapf {
         return m;
     }
 
-    private int[] kk = new int[42];
-
     public String theKruskal() {
         if (!isBiDirectional) {
             throw new IllegalStateException("Graph is not bidirectional");
@@ -182,11 +180,10 @@ public class Grapf {
 
         ArrayUtils.sortArraysLikeFirst(weights, fromArray, toArray);
 
+        final DSU dsu = new DSU(fromArray.length);
+        final int[] sTree = new int[42];
         int w = 0;
-        DSU dsu = new DSU(kk);
-        // TODO
-        // TODO
-        // TODO, TODO, TODO, TO-DOOOO
+
         for (int k = 0; k < fromArray.length && w < getCountOfNodes() - 1; k++) {
             int i = fromArray[k];
             int j = toArray[k];
@@ -195,7 +192,7 @@ public class Grapf {
             int mJ = dsu.findSet(j);
 
             if (mI != mJ) {
-                kk[w] = k;
+                sTree[w] = k;
                 w++;
                 dsu.unionSets(mI, mJ);
             }
@@ -204,27 +201,25 @@ public class Grapf {
         StringBuilder graph = new StringBuilder();
         graph.append("digraph {\n");
 
-        for (int i = 0; i < w; i++) { // TODO: что-нибудь хранить
-            if (kk[i] != NOTHING) {
-                int begin = fromArray[kk[i]];
-                int end = toArray[kk[i]];
-                int weight = weights[kk[i]];
+        for (int i = 0; i < w; i++) {
+            int begin = fromArray[sTree[i]];
+            int end = toArray[sTree[i]];
+            int weight = weights[sTree[i]];
 
-                graph.append("\t\t");
+            graph.append("\t\t");
 
-                graph.append(Integer.toString(begin));
-                graph.append(" -> ");
-                graph.append(Integer.toString(end));
+            graph.append(Integer.toString(begin));
+            graph.append(" -> ");
+            graph.append(Integer.toString(end));
 
-                graph
-                        .append("[label=\"")
-                        .append(weight)
-                        .append("\",weight=\"")
-                        .append(weight)
-                        .append("\"]");
+            graph
+                    .append("[label=\"")
+                    .append(weight)
+                    .append("\",weight=\"")
+                    .append(weight)
+                    .append("\"]");
 
-                graph.append(";\n");
-            }
+            graph.append(";\n");
         }
 
         graph.append("\t}");
@@ -236,9 +231,9 @@ public class Grapf {
         private int[] parents;
         private int[] rank;
 
-        public DSU(int[] parents) {
-            this.parents = parents;
-            this.rank = new int[parents.length];
+        public DSU(int size) {
+            this.parents = new int[size];
+            this.rank = new int[size];
             for (int i = 0; i < parents.length; i++) {
                 makeSet(i);
             }
